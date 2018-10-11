a761dee02a4dd12162eb55ef34cc0989c79567cc
Alex Beccaro <alex.becks@hotmail.it>
2018-10-11 10:52:02 +0200

    Added n-queens tests

--- tests/n_queens_tests.py
+++ tests/n_queens_tests.py
@@ -62,6 +62,62 @@ class NQueensTests(unittest.TestCase):
         pprint(solution)
         print(stats)
 
+    def test_20x20(self):
+        prob, vars = self._n_queens_problem(20)
+
+        solver = BacktrackSolver()
+
+        solved, stats = solver.solve(prob)
+
+        self.assertEqual(solved, True)
+
+        solution = [v.get_value() for v in vars]
+        print('\n20 queens:')
+        pprint(solution)
+        print(stats)
+
+    def test_40x40(self):
+        prob, vars = self._n_queens_problem(40)
+
+        solver = BacktrackSolver()
+
+        solved, stats = solver.solve(prob)
+
+        self.assertEqual(solved, True)
+
+        solution = [v.get_value() for v in vars]
+        print('\n40 queens:')
+        pprint(solution)
+        print(stats)
+
+    def test_60x60(self):
+        prob, vars = self._n_queens_problem(60)
+
+        solver = BacktrackSolver()
+
+        solved, stats = solver.solve(prob)
+
+        self.assertEqual(solved, True)
+
+        solution = [v.get_value() for v in vars]
+        print('\n60 queens:')
+        pprint(solution)
+        print(stats)
+
+    def test_80x80(self):
+        prob, vars = self._n_queens_problem(80)
+
+        solver = BacktrackSolver()
+
+        solved, stats = solver.solve(prob)
+
+        self.assertEqual(solved, True)
+
+        solution = [v.get_value() for v in vars]
+        print('\n80 queens:')
+        pprint(solution)
+        print(stats)
+
     def test_100x100(self):
         prob, vars = self._n_queens_problem(100)
 
@@ -81,6 +137,10 @@ if __name__ == '__main__':
     suite = unittest.TestSuite()
     suite.addTest(NQueensTests('test_2x2'))
     suite.addTest(NQueensTests('test_8x8'))
+    suite.addTest(NQueensTests('test_20x20'))
+    suite.addTest(NQueensTests('test_40x40'))
+    suite.addTest(NQueensTests('test_60x60'))
+    suite.addTest(NQueensTests('test_80x80'))
     suite.addTest(NQueensTests('test_100x100'))
 
     unittest.TextTestRunner().run(suite)
